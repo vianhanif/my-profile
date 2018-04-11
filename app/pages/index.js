299331b06084be784ec8720119501d5dd60481ac
Alvian Rahman Hanif <alvian524@gmail.com>
2018-04-11 14:48:04 +0700

    fix routes path

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { RouteWithSubRoutes } from '../shared/components'
-import routes from './routes'
+import routes from '@routes/'
 
 export default () => (
   <Switch>
